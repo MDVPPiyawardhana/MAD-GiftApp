7931fc8c1ccaf38a75f2b173ea1c6b7e0c9a74f7
Admin <pamuditha56@gmail.com>
2021-05-08 19:39:52 +0530

    shipping details - 01

---
+++ app/src/main/java/com/example/giftsoflove/Shipping_Details.java
@@ -0,0 +1,14 @@
+package com.example.giftsoflove;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shipping_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shipping_details);
+    }
+}
